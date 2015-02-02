c2f9843c74738ba97773193702338962471a7e7e
antonhansel <antoninribeaud@gmail.com>
2015-02-02 12:41:08 +0100

    Testint travis with a basic test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,22 @@
 'use strict';
 
 require('should');
-
-var hydrater = require('../lib/');
-
+var anyfetchHydrater = require('anyfetch-hydrater');
+var pdfocr = require('../lib/');
 
 describe('Test HYDRATER results', function() {
-  it('returns the correct informations', function(done) {
-    var document = {
-      datas: {}
-    };
-    done();
-  });
+	it('returns the correct informations', function(done) {
+		var document = {
+			datas: {}
+		};
+
+		var changes = anyfetchHydrater.defaultChanges();
+
+		pdfocr(__dirname + "./test.pdf", document, changes, function(err, changes){
+			if (err){
+				throw err;
+			}
+		})
+		done();
+	});
 });
