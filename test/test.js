7f4a586966a6cca985dd2dc69a323fce1c56f117
antonhansel <antoninribeaud@gmail.com>
2015-02-02 20:22:40 +0100

    Tests are now properly written

--- test/test.js
+++ test/test.js
@@ -2,27 +2,34 @@
 
 require('should');
 var anyfetchHydrater = require('anyfetch-hydrater');
-var pdfocr = require('../lib/');
+var fs = require('fs');
 
+var pdfocr = require('../lib/');
 var HydrationError = anyfetchHydrater.HydrationError;
 
 describe('Test PDFOCR results', function() {
   it('returns the correct informations', function(done) {
-        var document = {
-            datas: {}
-        };
+      var document = {
+          datas: {}
+      };
 
-        var changes = anyfetchHydrater.defaultChanges();
-
-        pdfocr(__dirname + "/samples/test.pdf", document, changes, function(err, changes){
-            if (err){
-                throw err;
+      var changes = anyfetchHydrater.defaultChanges();
+      pdfocr(__dirname + "/samples/test.pdf", document, changes, function(err, changes){
+          if (err){
+              throw err;
+          }
+          fs.readFile(__dirname + '/samples/dump.txt', function(err, res) {
+            if (err) {
+              throw err;
             }
+            changes.metadata.should.containDeep(res.toString());
+
+            done();
+          });
         });
-        done();
     });
 
-    it('should return an errored document', function(done) {
+  it('should return an errored document', function(done) {
     var document = {
       data: {}
     };
