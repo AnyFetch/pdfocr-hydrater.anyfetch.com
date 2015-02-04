995c7a40ed8902703f13032d38cceeb615b33315
antonhansel <antoninribeaud@gmail.com>
2015-02-04 12:11:56 +0100

    Padding image names in case there is more than 10 images and adding the test case

--- test/test.js
+++ test/test.js
@@ -8,17 +8,33 @@ var pdfocr = require('../lib/');
 var HydrationError = anyfetchHydrater.HydrationError;
 
 describe('Test PDFOCR results', function() {
-  it('returns the correct informations', function(done) {
+  it('returns the correct informations for a simple pdf file (2 images)', function(done) {
       var document = {
           datas: {}
       };
 
       var changes = anyfetchHydrater.defaultChanges();
-      pdfocr(__dirname + "/samples/test.pdf", document, changes, function(err, changes) {
+      pdfocr(__dirname + "/samples/test-2img.pdf", document, changes, function(err, changes) {
           if(err) {
             throw err;
           }
-          var data = fs.readFileSync(__dirname + '/samples/dump.txt');
+          var data = fs.readFileSync(__dirname + '/samples/dump-2img.txt');
+          changes.metadata.should.eql(data.toString());
+          done();
+        });
+    });
+
+  it('returns the correct informations for a complicated pdf file (11 images)', function(done) {
+      var document = {
+          datas: {}
+      };
+
+      var changes = anyfetchHydrater.defaultChanges();
+      pdfocr(__dirname + "/samples/test-11img.pdf", document, changes, function(err, changes) {
+          if(err) {
+            throw err;
+          }
+          var data = fs.readFileSync(__dirname + '/samples/dump-11img.txt');
           changes.metadata.should.eql(data.toString());
           done();
         });
