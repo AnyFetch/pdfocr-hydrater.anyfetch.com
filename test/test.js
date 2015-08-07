120f547817aa697090085378f589c635d36c3de8
Amoki <hugo.duroux@gmail.com>
2015-08-07 17:53:19 +0200

    fix kill process. Multiple instances are allowed

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ var HydrationError = anyfetchHydrater.HydrationError;
 describe('Test PDFOCR results', function() {
   it('returns the correct informations for a simple pdf file (2 images)', function(done) {
     var document = {
-        datas: {}
+      datas: {}
     };
 
     var changes = anyfetchHydrater.defaultChanges();
@@ -26,18 +26,18 @@ describe('Test PDFOCR results', function() {
 
   it('returns the correct informations for a complicated pdf file (11 images)', function(done) {
     var document = {
-        datas: {}
+      datas: {}
     };
 
     var changes = anyfetchHydrater.defaultChanges();
     pdfocr(__dirname + "/samples/test-11img.pdf", document, changes, function(err, changes) {
-        if(err) {
-          throw err;
-        }
-        var data = fs.readFileSync(__dirname + '/samples/dump-11img.txt');
-        changes.metadata.text.should.eql(data.toString());
-        done();
-      });
+      if(err) {
+        throw err;
+      }
+      var data = fs.readFileSync(__dirname + '/samples/dump-11img.txt');
+      changes.metadata.text.should.eql(data.toString());
+      done();
+    });
   });
 
   it('should return an errored document', function(done) {
